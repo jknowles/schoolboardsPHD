3634795de8145c8f59a4c0db4c1937da46d1206b
Jared Knowles <jknowles@gmail.com>
2013-06-02 22:13:49 -0500

    rebuild PDF

--- build.R
+++ build.R
@@ -8,16 +8,16 @@ system("bibtex dissertation")
 system("pdflatex dissertation")
 system("pdflatex dissertation")
 
-cleantex<-function(mydoc,keepPDF){
-  a<-list.files(pattern=mydoc)
-  save<-a[grep(".tex",a)]
+cleantex <- function(mydoc,keepPDF){
+  a <- list.files(pattern=mydoc)
+  save <- a[grep(".tex",a)]
   if(keepPDF==TRUE){  
-    save<-append(save,a[grep(".pdf",a)])
+    save <- append(save,a[grep(".pdf",a)])
   }
   else if(keepPDF==FALSE){
-    save<-a[grep(".tex",a)]
+    save <- a[grep(".tex",a)]
   }
-  rm<-setdiff(a,save)
+  rm <- setdiff(a,save)
   file.remove(rm)
 }
 
