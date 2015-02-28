d27fa68ce6ab71f5637af2d8cbd51900136e5950
Jared Knowles <jknowles@gmail.com>
2015-02-28 15:53:04 -0600

    new chapter 5 results incorporated

--- build.R
+++ build.R
@@ -57,19 +57,3 @@ cleantex <- function(mydoc,keepPDF){
 }
 
 cleantex("dissertation", keepPDF=TRUE)
-
-# stackoverflow.com/questions/7512897/how-to-turn-a-latex-sweave-file-rnw-into-html
-# Need imagemagick
-# choco install imagemagick.app
-# SWconvurl <- "http://biostat.mc.vanderbilt.edu/wiki/pub/Main/SweaveConvert/sweave2html"
-# download.file(SWconvurl,dest="bin/sweave2html")
-#  Sys.chmod(c("tth_exe","sweave2html"),mode="0755") ## ???
-# system("bin/sweave2html chapters/aboutwisconsin/aboutwisconsin.tex")
-# system("tth -a -d /chapters/aboutwisconsin.tex")
-## Make word docs
-# pandoc -s math.tex -o example30.docx
-# # http://tex.stackexchange.com/questions/186606/partial-compile-latex-and-bibtex-for-pandoc 
-# setwd("chapters")
-# setwd("aboutwisconsin")
-# system("pandoc -s aboutwisconsin.tex -o aboutwisconsin.docx") 
-# system("pandoc -o word.docx --bibliography ../../bib/candidcay.bib --csl ../../bib/apsr.csl original.tex")
