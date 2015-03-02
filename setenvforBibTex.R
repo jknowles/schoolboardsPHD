4615fc486ed936e06d475e60ecab02d082bdc951
Jared Knowles <jknowles@gmail.com>
2015-03-02 00:55:39 -0600

    data section almost done

--- setenvforBibTex.R
+++
@@ -1,6 +0,0 @@
-#Set Environment Variables
-TEXINPUTS="C:\\Users\\Jared\\Dropbox\\
-           Dissertation\\MasterText"
-
-Sys.setenv(TEXINPUTS="C:\\Users\\Jared\\Dropbox\\Dissertation\\MasterText",
-           BIBINPUTS=paste(TEXINPUTS,"\\bib",sep=""),BSTINPUTS=paste(TEXINPUTS,"\\bib",sep=""))
